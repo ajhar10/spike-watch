e540a08eaf092eca657618f4c8fef157a6e78630
ajhar10 <ajharrimon90@gmail.com>
2022-04-05 16:18:07 +0600

    README fie edit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <>
+            <h1 className='text-4xl text-teal-700 font-medium text-center'>Hello I am a web developer.</h1>
         </>
     );
 };
